127c7fb8e4f114d3127616ad906ea896712e5986
jordannares <jordannares21@gmail.com>
2025-08-05 14:28:08 +0800

    remove page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Link, Route, Routes } from "react-router-dom";
 
 import NotFound from "./pages/NotFound";
-import Navbar from "./layouts/navbar/Navbar";
 import AboutUs from "./pages/about";
 import ContactUs from "./pages/contact";
 import Home from "./pages/home";
@@ -14,7 +13,6 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
